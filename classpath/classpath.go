28d764fb57348777e831e74ea1768ccfc103ec6d
LILINJIAN <shinozaki9852@gmail.com>
2026-06-02 00:07:18 +0900

    Update Classpath methods: implement `ReadClass`, address file suffixes and paths for clarity and maintainability

--- classpath/classpath.go
+++ classpath/classpath.go
@@ -21,24 +21,44 @@ func Parse(jreOption string, cpOption string) *Classpath {
 }
 
 func (c *Classpath) String() string {
-	return ""
+	return c.userClassPath.toString()
 }
 
+// ReadClass 依次从boot ext user类路径搜索class文件
 func (c *Classpath) ReadClass(className string) ([]byte, Entry, error) {
-	return nil, nil, nil
+	className = className + classFileSuffix
+
+	byteData, entry, err := c.bootClassPath.readClass(className)
+	if err == nil {
+		return byteData, entry, err
+	}
+	byteData, entry, err = c.extClassPath.readClass(className)
+	if err == nil {
+		return byteData, entry, err
+	}
+	byteData, entry, err = c.userClassPath.readClass(className)
+	if err == nil {
+		return byteData, entry, err
+	}
+	return nil, nil, err
 }
 
+// parseBootAndExtClassPath 基于 JRE 目录设置启动和扩展类路径条目。
 func (c *Classpath) parseBootAndExtClassPath(jreOption string) {
 	jreDir := getJreDir(jreOption)
 	// jre/lib/*
-	jreLibDir := filepath.Join(jreDir, "lib", wildcardClasspathSuffix)
+	jreLibDir := filepath.Join(jreDir, jreLibDirName, wildcardClasspathSuffix)
 	c.bootClassPath = newWildcardEntry(jreLibDir)
 	// jre/lib/ext/*
-	jreExtDir := filepath.Join(jreDir, "lib", "ext", wildcardClasspathSuffix)
+	jreExtDir := filepath.Join(jreDir, jreLibDirName, jreExtDirName, wildcardClasspathSuffix)
 	c.extClassPath = newWildcardEntry(jreExtDir)
 }
 
 func (c *Classpath) parseUserClassPath(cpOption string) {
+	if cpOption == blank {
+		cpOption = defaultUserClasspath
+	}
+	c.userClassPath = newDirEntry(cpOption)
 }
 
 // 优先使用用户输入的-Xjre选项作为jre目录
@@ -48,13 +68,13 @@ func getJreDir(jreOption string) string {
 	if jreOption != blank && exists(jreOption) {
 		return jreOption
 	}
-	if exists("./jre") {
-		return "./jre"
+	if exists(defaultJreDir) {
+		return defaultJreDir
 	}
-	if javaHome := os.Getenv("JAVA_HOME"); javaHome != blank {
-		return filepath.Join(javaHome, "jre")
+	if javaHome := os.Getenv(javaHomeEnv); javaHome != blank {
+		return filepath.Join(javaHome, jreDirName)
 	}
-	panic("Can not find jre directory, please input -Xjre option or set JAVA_HOME environment variable")
+	panic(jreDirNotFoundMessage)
 }
 
 // 用于判断路径是否存在
